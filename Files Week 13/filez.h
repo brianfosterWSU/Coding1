08ac60a02d0ee4b6790b44dcca77c24f1a8338e8
Brian Foster <brianisbrilliant@users.noreply.github.com>
2023-04-26 11:26:53 -0500

    added multiple files from week 13

---
+++ Files Week 13/filez.h
@@ -0,0 +1,7 @@
+#include <fstream>
+using namespace std;
+
+void SaveTextToNewFile();
+void AppendToEndOfFile(string text);
+void CheckingOpenStatus(string filename = "file.txt");
+void ReadingFromFile(string filename = "data.txt");
